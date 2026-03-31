ea4699a38245ea45e8bc92f6ae1fef89e5bb3742
lifujian <lifujian@meituan.com>
2026-03-31 19:24:14 +0800

    test: add save/load tests for StockUniverseFetcher

--- java/src/test/java/fetcher/StockUniverseFetcherTest.java
+++ java/src/test/java/fetcher/StockUniverseFetcherTest.java
@@ -1,5 +1,6 @@
 package fetcher;
 
+import org.junit.Rule;
 import org.junit.Test;
 import java.util.List;
 import static org.junit.Assert.*;
@@ -39,5 +40,32 @@ public class StockUniverseFetcherTest {
         assertEquals("sh600519", entry.symbol);
         assertEquals("贵州茅台", entry.name);
         assertEquals(2100000000000L, entry.marketCap);
+    }
+
+    @Rule
+    public org.junit.rules.TemporaryFolder tmp = new org.junit.rules.TemporaryFolder();
+
+    @Test
+    public void testSaveAndLoad() throws Exception {
+        java.io.File outputFile = new java.io.File(tmp.getRoot(), "stock-universe.json");
+        java.util.List<StockUniverseFetcher.StockEntry> stocks = java.util.Arrays.asList(
+            new StockUniverseFetcher.StockEntry("sh600519", "贵州茅台", 2100000000000L),
+            new StockUniverseFetcher.StockEntry("sz000858", "五粮液", 580000000000L)
+        );
+        StockUniverseFetcher.save(stocks, outputFile.getAbsolutePath());
+
+        java.util.List<StockUniverseFetcher.StockEntry> loaded =
+            StockUniverseFetcher.load(outputFile.getAbsolutePath());
+
+        assertEquals(2, loaded.size());
+        assertEquals("sh600519", loaded.get(0).symbol);
+        assertEquals("贵州茅台", loaded.get(0).name);
+        assertEquals(2100000000000L, loaded.get(0).marketCap);
+        assertEquals("sz000858", loaded.get(1).symbol);
+    }
+
+    @Test(expected = java.io.FileNotFoundException.class)
+    public void testLoadThrowsWhenFileNotFound() throws Exception {
+        StockUniverseFetcher.load("/nonexistent/path/stock-universe.json");
     }
 }
